0877cf101c3940fda4d7c4c59da95711dbe3dcc4
diogenes <diogerafael@gmail.com>
2022-05-23 12:19:09 -0300

    Adicionando busca de usuário por ID

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,19 +1,32 @@
 package com.educandoweb.course.resources;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> users(){
-        //User u = new User(1, "Diogenes", "88888", "12345");
-        User u = new User();
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> users(){
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> usersById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
